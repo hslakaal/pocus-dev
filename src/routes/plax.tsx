dc10a64e067e90f884bf0c21ed97769e2214de83
hyuns <hyun.lee@doctors.org.uk>
2026-09-03 12:56:03 -0400

    Added swipeable images, and aa dropdown box

--- src/routes/plax.tsx
+++ src/routes/plax.tsx
@@ -11,7 +11,7 @@ import { useEchoStore } from "@/lib/use-echo-store";
 
 // Defining array
 
-const plax_images = ["public/images/plax/PLAX.gif", "public/images/plax/plax labelled.png"];
+const plax_images = ["public/images/plax/plax.gif", "public/images/plax/plax labelled.png"];
 
 export const Route = createFileRoute("/plax")({
   head: () => ({
