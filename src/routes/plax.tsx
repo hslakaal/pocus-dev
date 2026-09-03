5b1b37c8349e37b015e830fbf239d1092d921082
hslakaal <hyun.lee@doctors.org.uk>
2026-09-03 13:04:17 -0400

    Update plax.tsx

--- src/routes/plax.tsx
+++ src/routes/plax.tsx
@@ -11,7 +11,7 @@ import { useEchoStore } from "@/lib/use-echo-store";
 
 // Defining array
 
-const plax_images = ["public/images/plax/PLAX.gif", "public/images/plax/plax labelled.png"];
+const plax_images = ["/images/plax/PLAX.gif", "/images/plax/plax labelled.png"];
 
 export const Route = createFileRoute("/plax")({
   head: () => ({
